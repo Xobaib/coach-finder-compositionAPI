67b522ba2be0df780165c1a187291693fed032b8
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-08-03 14:39:32 +0430

    10- Working on the Coaches List and List Items

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { computed, defineProps } from 'vue';
+
+const props = defineProps(['id', 'firstName', 'lastName', 'rate', 'areas']);
+
+const fullName = computed(() => {
+  return `${props.firstName} ${props.lastName}`;
+});
+
+const coachContactLink = computed(() => {
+  return `/coaches/${props.id}/contact`; // /coaches/c1/contact
+});
+
+const coachDetailsLink = computed(() => {
+  return `/coaches/${props.id}`; // /coaches/c1
+});
+</script>
+
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <RouterLink :to="coachContactLink">Contact</RouterLink>
+      <RouterLink :to="coachDetailsLink">Details</RouterLink>
+    </div>
+  </li>
+</template>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -2,6 +2,8 @@
 import { computed } from 'vue';
 import { useStore } from 'vuex';
 
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 const store = useStore();
 
 const filteredCoaches = computed(() => {
@@ -21,10 +23,29 @@ const hasCoaches = computed(() => {
       <RouterLink to="/register">Register as Coach</RouterLink>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <CoachItem
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      />
     </ul>
     <h3 v-else>No coaches Found!</h3>
   </section>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
