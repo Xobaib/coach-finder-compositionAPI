51b99336a30178ea4c77b726103367a46e3cc3d8
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-08-18 14:44:23 +0430

    14 Registering as a Coach The Form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,12 @@
-<template>REGISTER</template>
+<script setup>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+</script>
+
+<template>
+  <section>
+    <BaseCard>
+      <h2>Register as a coach now!</h2>
+      <CoachForm></CoachForm>
+    </BaseCard>
+  </section>
+</template>
