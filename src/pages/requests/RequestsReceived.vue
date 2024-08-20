9c74efea262234db8a8be8832751e315a40588f5
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-08-20 18:02:05 +0430

    19 Outputting Incoming Requests (Messages)

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,1 +1,51 @@
-<template>REQUESTS</template>
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import RequestItem from '../../components/requests/RequestItem.vue';
+
+const store = useStore();
+
+const receivedRequsets = computed(() => {
+  return store.getters['requests/requests'];
+});
+
+const hasRequests = computed(() => {
+  return store.getters['requests/hasRequests'];
+});
+</script>
+
+<template>
+  <section>
+    <BaseCard>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <RequestItem
+          v-for="request in receivedRequsets"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.message"
+        />
+      </ul>
+      <h3 v-else>You haven'treceived any requests yet!</h3>
+    </BaseCard>
+  </section>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
