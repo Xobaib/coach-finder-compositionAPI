2aa434fcf333f9e3a22584a879263c73018ec299
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-08-20 14:10:17 +0430

    17 Working on the Contact Form

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,1 +1,83 @@
-<template>CONTACT COACH</template>
+<script setup>
+import { ref } from 'vue';
+
+const email = ref('');
+const message = ref('');
+const formIsVaild = ref(true);
+
+function submitForm() {
+  formIsVaild.value = true;
+
+  if (
+    email.value === '' ||
+    !email.value.includes('@') ||
+    message.value === ''
+  ) {
+    formIsVaild.value = false;
+    return;
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your E-Mail</label>
+      <input type="email" id="email" v-model.trim="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <textarea rows="5" id="message" v-model.trim="message"></textarea>
+    </div>
+    <h3 class="errors" v-if="!formIsVaild">
+      Please enter a valid email and non empty message.
+    </h3>
+    <div class="actions">
+      <BaseButton>Send Message</BaseButton>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
